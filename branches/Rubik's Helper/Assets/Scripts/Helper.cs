234af67b1c08fa9546309288eef7e822ad6b5292
im11706 <im11706@731a7f06-d8f9-4fd2-b2dc-3da9ebbc610b>
2023-03-10 23:53:09 +0000

    Amélioration de la clase Helper

--- branches/Rubik's Helper/Assets/Scripts/Helper.cs
+++ branches/Rubik's Helper/Assets/Scripts/Helper.cs
@@ -1,34 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
+
+
 
 public class Helper : MonoBehaviour
 {
-    public GameObject Panel;
-    //private Text myText;
+    public GameObject PanelHelper;
     public GameObject Toggle;
-    public PivotRotation pivotRotation;
+    public Text indicationClic;
+    public Text indicationRelache;
 
-    //public RubiksFaces rubiksFaces;
-    public Text indication;
+
     public void AfficheAide()
     {
         if (Toggle.GetComponent<Toggle>().isOn)
         {
-            Panel.SetActive(true);
-            if (pivotRotation.IsFunctionRun()==false)
-            {
-                indication.text = "L'aide s'affiche";
-            }
-            else 
-            {
-                indication.text = "La fonction est en cours d'utilisation";
-            }
+            PanelHelper.SetActive(true);
         }
         else
         {
-            Panel.SetActive(false);
+            PanelHelper.SetActive(false);
         }
     }
+    
+
+    public void OnPointerDown()
+    {
+        indicationClic.gameObject.SetActive(true);
+        indicationRelache.gameObject.SetActive(false);
+        indicationClic.text = "Vous avez cliqué sur l'écran";
+    }
+
+    public void OnPointerUp()
+    {
+        indicationClic.gameObject.SetActive(false);
+        indicationRelache.gameObject.SetActive(true);
+        indicationRelache.text = "Vous avez relâché la souris";
+    }
+
+    void Update()
+    {
+        if (PanelHelper.activeSelf && Input.GetMouseButtonDown(0))
+        {
+            OnPointerDown();
+        }
+        
+        if (PanelHelper.activeSelf && Input.GetMouseButtonUp(0))
+        {
+            OnPointerUp();
+        }
+    }
+    
 }
