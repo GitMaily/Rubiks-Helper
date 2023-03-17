b9b8a9124b6c25ae0edf6af62e984a3d505717f3
il01193 <il01193@731a7f06-d8f9-4fd2-b2dc-3da9ebbc610b>
2023-03-17 20:40:09 +0000

    Test

--- branches/Assets/Scripts/AideResolution.cs
+++ branches/Assets/Scripts/AideResolution.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Class test pour Helper
+/// </summary>
 public class AideResolution : MonoBehaviour
 {
     // Start is called before the first frame update
